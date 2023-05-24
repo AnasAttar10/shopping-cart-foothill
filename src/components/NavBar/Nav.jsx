307b6ad0e9a8ac970176477c0e4d0ccc5952103a
AnasAttar10 <anasattar2010@gmail.com>
2023-05-24 16:09:48 +0300

    update the navbar

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import style from './nav.module.css'
 import { useShoppingCart } from '../../Context/shopppingcartContext'
 const Nav = () => {
   const {toggleVisiblityMyCart , getTotalProducts} =useShoppingCart()
+  const {pathname} = useLocation()
+  console.log(pathname);
   const totalProducts =getTotalProducts()
   return (
     <div className={style.container}>
@@ -19,11 +21,14 @@ const Nav = () => {
                     <NavLink className={(navData) => navData.isActive ? style.active : style.navLink} to="contact">Contact</NavLink>
                 </li>
             </ul>
+            {
+            pathname==="/store"&&
             <div>
               <i className={"fa-sharp fa-solid fa-cart-shopping " + style.cartIcon } onClick={()=>toggleVisiblityMyCart()}>
                 <span className={style.totalProducts}>{totalProducts || 0}</span>
               </i>
-              </div>
+            </div>
+            }
     </nav>
     </div>
   )
